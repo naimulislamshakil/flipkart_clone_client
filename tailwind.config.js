d4f29872a687764197b767a20da9784e4745d9ca
naimulislamshakil <ms0413354@gmail.com>
2022-08-16 22:46:34 +0600

    complete header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#2874fo",
+          primary: "#0066ff",
           secondary: "#f6d860",
           accent: "#37cdbe",
           neutral: "#3d4451",
